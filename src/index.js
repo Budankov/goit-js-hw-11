617b2190ec34833672041e4c689ee5c8f1437dea
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2022-12-05 17:40:58 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -4,3 +4,13 @@ import { fetchImages } from './js/fetchImages';
 
 const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
+
+const searchForm = document.querySelector('#search-form');
+const searchBtn = document.querySelector('.search-button');
+
+searchForm.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
+
+function onInputChange(e) {
+  const { value } = e.target;
+  console.log(value);
+}
