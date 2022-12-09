f20d2682fd5027c63e29569cc0aba2f60836e054
Budankov Kostya <107200706+Budankov@users.noreply.github.com>
2022-12-09 21:17:35 +0200

    Update

--- src/index.js
+++ src/index.js
@@ -8,17 +8,44 @@ import { createMarkupElemetsGallery } from './js/createMarkupElemetsGallery';
 const pixabayApi = new PixabayApi();
 
 const searchForm = document.querySelector('#search-form');
-const searchBtn = document.querySelector('.search-button');
 const galleryListEl = document.querySelector('.gallery');
+const galleryEnd = document.querySelector('.gallary-end');
 
 searchForm.addEventListener('submit', onSearchImages);
 
 let galleryImages = [];
 
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 1.0,
+};
+
+const callback = function (entries) {
+  const line = entries[0].isIntersecting;
+  console.log(line);
+
+  if (line) {
+    pixabayApi.page += 1;
+    renderGallery();
+  }
+};
+
+const observer = new IntersectionObserver(callback, options);
+
+observer.observe(galleryEnd);
+
+// const observer = new IntersectionObserver(async enteries => {
+//   if (enteries[0].isIntersecting) {
+//     pixabayApi.page += 1;
+//     renderGallery();
+//   }
+// }, observerOptions);
+
 function renderGallery() {
   const galleryEl = galleryImages.map(createMarkupElemetsGallery);
 
-  galleryListEl.innerHTML = '';
+  // galleryListEl.innerHTML = '';
   galleryListEl.insertAdjacentHTML('beforeend', galleryEl.join(''));
   const gallery = new SimpleLightbox('.gallery a');
 }
@@ -35,6 +62,7 @@ async function onSearchImages(e) {
 
     const { data } = await pixabayApi.fetchPhoto();
     Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+    galleryListEl.innerHTML = '';
     galleryImages = data.hits;
     renderGallery();
   } catch (error) {
